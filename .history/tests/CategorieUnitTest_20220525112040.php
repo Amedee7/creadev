d7600d286a551471af105ad687d565230b5faee9
Amedee7 <amedeeouedraogo01@gmail.com>
2022-05-27 11:38:17 +0000

    Ajout des entités et test

---
+++ .history/tests/CategorieUnitTest_20220525112040.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Categorie;
+use PHPUnit\Framework\TestCase;
+
+class CategorieUnitTest extends TestCase
+{
+    public function testIsTrue()
+    {
+        $categorie = new Categorie();
+        $categorie->setNom('nom')
+            ->setDescription('description')
+            ->setSlug('slug');
+
+        $this->assertTrue($categorie->getNom() === 'nom');
+        $this->assertTrue($categorie->getDescription() === 'description');
+        $this->assertTrue($categorie->getSlug() === 'slug');
+    }
+
+    public function testIsFalse()
+    {
+        $categorie = new Categorie();
+        $categorie->setNom('nom')
+            ->setDescription('description')
+            ->setSlug('slug');
+
+        $this->assertFalse($categorie->getNom() === 'false');
+        $this->assertFalse($categorie->getDescription() === 'false');
+        $this->assertFalse($categorie->getSlug() === 'false');
+    }
+    public function testIsEmpty()
+    {
+        $categorie = new Categorie();
+
+        $this->assertEmpty($categorie->getNom());
+        $this->assertEmpty($categorie->getDescription());
+        $this->assertEmpty($categorie->getSlug());
+        $this->assertEmpty($categorie->getId());
+    }
+
+    // public function testAddGetRemovePeinture()
+    // {
+    //     $categorie = new Categorie();
+    //     $peinture = new Peinture();
+
+    //     $this->assertEmpty($categorie->getPeintures());
+
+    //     $categorie->addPeinture($peinture);
+    //     $this->assertContains($peinture, $categorie->getPeintures());
+
+    //     $categorie->removePeinture($peinture);
+    //     $this->assertEmpty($categorie->getPeintures());
+    // }
+}
